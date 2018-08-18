60bc5f50efa8f40891875e00b7219c8751f16139
redell <382663074@qq.com>
2018-08-18 14:31:16 +0800

    引入canvas-nest背景图片，不过有bug

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -2,16 +2,21 @@
   <div class="index-wrap">
      <div class="title">
        <p>欢迎来到Vue-antd管理系统</p>
+       <p style="font-size: 30px; color:#f3b7b7;">Author: redell</p>
+       <p style="font-size: 30px;"><a style="color:#f3b7b7;" href="https://github.com/GongJS/vue-antd-management">Github: GongJS</a></p>
      </div>
-    <img src="../../../static/vue.png" alt="" >
+     <canvas-nest></canvas-nest>
   </div>
 </template>
 <script>
+import CanvasNest from '@/components/CanvasNest/CanvasNest'
 export default {
   name: 'Index',
+  components: {
+    'canvas-nest': CanvasNest
+  },
   data () {
     return {
-
     }
   }
 }
@@ -22,28 +27,21 @@ export default {
   .index-wrap{
     background-color: @colorM;
     height: calc(~"100vh - 240px");
-    display: flex;
-    align-items: center;
-    justify-content: center;
     font-size: 20px;
     position: relative;
     .title {
-      position: absolute;
       text-align: center;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      width:500px;
+      height:200px;
+      margin-left: -250px;
+      margin-top: -100px;
       color: white;
       text-shadow: 2px 2px 4px #000000;
       font-size: 32px;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    img {
-      width: 100%;
-      height: 100%;
+      z-index: 2;
     }
   }
 </style>
